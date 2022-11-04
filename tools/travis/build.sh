b8a1f53a60051d9b132e36d57372aeca0c0ea9e1
Luke Roy <luke.roy@ibm.com>
2022-11-04 14:32:04 +0100

    Test code github action

---
+++ tools/travis/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -ex
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+
+export OPENWHISK_HOME=$WHISKDIR
+
+IMAGE_PREFIX="testing"
+
+# Login to hub.docker.com to get user specific pull rate.
+if [ ! -z "${DOCKER_USER}" ] && [ ! -z "${DOCKER_PASSWORD}" ]; then
+  echo "Run docker login..."
+  echo ${DOCKER_PASSWORD} | docker login -u "${DOCKER_USER}" --password-stdin
+fi
+
+# Build OpenWhisk
+cd $WHISKDIR
+
+#pull down images
+docker pull ibmfunctions/controller:nightly
+docker tag ibmfunctions/controller:nightly ${IMAGE_PREFIX}/controller
+docker pull ibmfunctions/invoker:nightly
+docker tag ibmfunctions/invoker:nightly ${IMAGE_PREFIX}/invoker
+
+TERM=dumb ./gradlew install
+
+# Build runtime
+cd $ROOTDIR
+TERM=dumb ./gradlew distDocker -PdockerImagePrefix=${IMAGE_PREFIX}
---
+++ tools/travis/publish.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eux
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+
+export OPENWHISK_HOME=$WHISKDIR
+
+IMAGE_PREFIX=$1
+RUNTIME_VERSION=$2
+IMAGE_TAG=$3
+
+if [ ${RUNTIME_VERSION} == "10" ]; then
+  RUNTIME="nodejs10"
+elif [ ${RUNTIME_VERSION} == "12" ]; then
+  RUNTIME="nodejs12"
+elif [ ${RUNTIME_VERSION} == "14" ]; then
+  RUNTIME="nodejs14"
+elif [ ${RUNTIME_VERSION} == "16" ]; then
+  RUNTIME="nodejs16"
+fi
+
+if [[ ! -z ${DOCKER_USER} ]] && [[ ! -z ${DOCKER_PASSWORD} ]]; then
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
+fi
+
+if [[ ! -z ${RUNTIME} ]]; then
+TERM=dumb ./gradlew \
+${RUNTIME}:pushImage \
+-PdockerRegistry=docker.io \
+-PdockerImagePrefix=${IMAGE_PREFIX} \
+-PdockerImageTag=${IMAGE_TAG}
+fi
